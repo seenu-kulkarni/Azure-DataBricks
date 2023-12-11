e7da8b657ac5ff85356e4ae5fddba43bb189bac7
seenu-kulkarni <seenu-kulkarni>
2023-12-11 15:14:05 +0000

    ingestion constructor json file to parquet

--- Formula1/3)Ingest Constructor.json file.py
+++ Formula1/3)Ingest Constructor.json file.py
@@ -23,4 +23,36 @@ display(constructor_df)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Step 2 - Drop unwanted columns from the dataframe
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+constructor_dropped_df = constructor_df.drop(col('url'))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Step 3 - Rename columns and add ingestion date
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+constructor_final_df = constructor_dropped_df.withColumnRenamed("constructorId", "constructor_id") \
+                                             .withColumnRenamed("constructorRef", "constructor_ref") \
+                                             .withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+constructor_final_df.write.mode("overwrite").parquet("/mnt/formula1dl/processed/constructors")
+
+# COMMAND ----------
+
 
