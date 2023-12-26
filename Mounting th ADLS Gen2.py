b4d513bbf00b22e852582ed447d1bb787531e125
seenu-kulkarni <seenu-kulkarni>
2023-12-26 13:51:54 +0000

    updated mounting

--- Mounting th ADLS Gen2.py
+++ Mounting th ADLS Gen2.py
@@ -5,11 +5,11 @@ client_secret = 'M6i8Q~zglJB5u~4TuMWwu-vVnLyiKwbuAV3Sjbwh'
 
 # COMMAND ----------
 
-spark.conf.set("fs.azure.account.auth.type.formula1stor.dfs.core.windows.net", "OAuth")
-spark.conf.set("fs.azure.account.oauth.provider.type.formula1stor.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.formula1stor.dfs.core.windows.net", client_id)
-spark.conf.set("fs.azure.account.oauth2.client.secret.formula1stor.dfs.core.windows.net", client_secret)
-spark.conf.set("fs.azure.account.oauth2.client.endpoint.formula1stor.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenent_id}/oauth2/token")
+spark.conf.set("fs.azure.account.auth.type.adlsgen2formula1.dfs.core.windows.net", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type.adlsgen2formula1.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id.adlsgen2formula1.dfs.core.windows.net", client_id)
+spark.conf.set("fs.azure.account.oauth2.client.secret.adlsgen2formula1.dfs.core.windows.net", client_secret)
+spark.conf.set("fs.azure.account.oauth2.client.endpoint.adlsgen2formula1.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenent_id}/oauth2/token")
 
 # COMMAND ----------
 
@@ -23,18 +23,22 @@ configs = {"fs.azure.account.auth.type": "OAuth",
 # COMMAND ----------
 
 dbutils.fs.mount(
-  source = "abfss://raw@formula1stor.dfs.core.windows.net/",
-  mount_point = "/mnt/formula1stor/raw",
+  source = "abfss://raw@adlsgen2formula1.dfs.core.windows.net/",
+  mount_point = "/mnt/adlsgen2formula1/raw",
   extra_configs = configs)
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("/mnt/formula1dl/raw"))
+display(dbutils.fs.ls("/mnt/adlsgen2formula1/raw"))
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("abfss://raw@formula1stor.dfs.core.windows.net"))
+display(dbutils.fs.ls("abfss://raw@adlsgen2formula1.dfs.core.windows.net"))
 
 # COMMAND ----------
 
-display(spark.read.csv("/mnt/formula1stor/raw/circuits.csv"))
+display(spark.read.csv("/mnt/adlsgen2formula1/raw/circuits.csv"))
+
+# COMMAND ----------
+
+
